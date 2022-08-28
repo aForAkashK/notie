df0e156d09d74e0412b66af2bb7534e0e1c2cea4
Akash Kumar <mailmeatakashkr@gmail.com>
2022-08-29 00:46:43 +0530

    Sign In Api / Register Api is done

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,19 +1,33 @@
 import UserModel from "../../server/models/register.model"
 import connectDb from "../../server/middleware/mongoose"
+var CryptoJS = require("crypto-js");
+var jwt = require('jsonwebtoken')
+
 
 const handler = async (req, res) => {
     if (req.method == "POST") {
-        for (let i = 0; i < req.body.length; i++) {
-            var UserData = new UserModel({
-                email: req.body[i].email,
-                password: req.body[i].password
-            })
+        var user = await UserModel.findOne({ email: req.body.email });
+        if (!user) {
+            if (req.body.email == "" || req.body.email == null || req.body.email == undefined) {
+                res.status(400).json({ Success: false, message: "Email is required" })
+            } else if (req.body.password == "" || req.body.password == null || req.body.password == undefined) {
+                res.status(400).json({ Success: false, message: "password is required" })
+            } else {
+                var UserData = new UserModel({
+                    email: req.body.email,
+                    password: CryptoJS.AES.encrypt(req.body.password, process.env.secret_key).toString()
+                })
+                var savedData = await UserData.save()
+                var payload = { subject: savedData._id }
+                var token = jwt.sign(payload, process.env.secret_key);
+                res.status(200).json({ Success: true, data: UserData, message: "Successfully Registered", token })
+            }
+        } else {
+            res.status(404).json({ Success: false, message: "User already Exists" })
         }
-        await UserData.save()
-        res.status(200).json({ Success: "true", data: UserData })
     }
     else {
-        res.status(400).json({ error: "This method is not allowed" })
+        res.status(400).json({ Success: false, message: `${req.method} method is not allowed` })
     }
 }
 
